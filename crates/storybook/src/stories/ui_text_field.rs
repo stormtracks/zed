4dfe2974c982a5d7b711deeca550ac100f79e094
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-04-24 09:29:27 -0700

    add in a second child to ui_text_field.rs to get some perspective on how the children are layed out

--- crates/storybook/src/stories/ui_text_field.rs
+++ crates/storybook/src/stories/ui_text_field.rs
@@ -16,15 +16,32 @@ impl Render for UiTextFieldStory {
             "UiTextField Story",
             "crates/storybook/src/stories/ui_text_field.rs",
         )
-        .children(vec![StorySection::new().child(
-            StoryItem::new("Default", div().bg(gpui::blue()).child("Hello World!")).usage(
+        .children(
+        vec![
+
+        StorySection::new()
+
+        .child(
+            StoryItem::new("Default",
+                div().bg(gpui::blue()).child("Hello World!")).usage(
                 indoc! {r##"
                             div()
                                 .child("Hello World!")
                             "##
                 },
             ),
-        )])
+        )
+        .child(
+            StoryItem::new("tbd",
+            div().flex().w_96().bg(gpui::blue()).child(div().overflow_hidden().child(concat!(
+                    "flex-row. width 96. overflow-hidden. The quick brown fox jumps over the lazy dog. ",
+                    "Meanwhile, the lazy dog decided it was time for a change. ",
+                    "He started daily workout routines, ate healthier and became the fastest dog in town.",
+                )))
+            )
+        )
+        ]
+        )
         .into_element()
     }
 }
