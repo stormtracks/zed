6cf6eaad02ec605283ccc663126173b3e2f651c4
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-05-01 08:56:54 -0700

    start to work on understanding ActionA

--- crates/gpui/examples/keyevent.rs
+++ crates/gpui/examples/keyevent.rs
@@ -4,10 +4,15 @@ struct HelloWorld {
     text: SharedString,
 }
 
+actions!(image, [ActionA, Quit]);
+
 impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
+            .on_action(cx.listener(|_, _action: &ActionA, _cx| {
+                println!("Action A dispatched on parent");
+            }))
             .bg(rgb(0x2e7d32))
             .size(Length::Definite(Pixels(300.0).into()))
             .justify_center()
@@ -21,15 +26,17 @@ impl Render for HelloWorld {
     }
 }
 
-actions!(image, [Quit]);
-
 fn main() {
     App::new().run(|cx: &mut AppContext| {
         cx.activate(true);
         cx.on_action(|_: &Quit, cx| cx.quit());
-        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+
+        cx.bind_keys([
+            KeyBinding::new("cmd-a", ActionA, None),
+            KeyBinding::new("cmd-q", Quit, None),
+        ]);
         cx.set_menus(vec![Menu {
-            name: "Image",
+            name: "Key",
             items: vec![MenuItem::action("Quit", Quit)],
         }]);
 
