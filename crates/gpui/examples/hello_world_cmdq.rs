25aba6cefe7b75f434c5b9d3b9b8a39efb28c498
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-05-06 09:37:17 -0700

    hello_world is working with cmd-q right off the bat with the code from PR #9158

---
+++ crates/gpui/examples/hello_world_cmdq.rs
@@ -0,0 +1,45 @@
+use gpui::*;
+
+struct HelloWorld {
+    text: SharedString,
+}
+
+actions!(hw, [Quit]);
+
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size(Length::Definite(Pixels(300.0).into()))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+    }
+}
+
+fn main() {
+    App::new().run(|cx: &mut AppContext| {
+        cx.activate(true);
+        cx.on_action(|_: &Quit, cx| cx.quit());
+        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+        let options = WindowOptions {
+            bounds: WindowBounds::Fixed(Bounds {
+                size: size(px(600.0), px(600.0)).into(),
+                origin: Default::default(),
+            }),
+            center: true,
+            ..Default::default()
+        };
+        cx.open_window(options, |cx| {
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        });
+    });
+}
