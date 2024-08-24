b4b004bb8107e4daecd92ddb1ac0cc715959dc85
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-08-23 19:42:22 -0700

    mw

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -1,10 +1,6 @@
 mod app_menus;
-//pub mod inline_completion_registry;
-#[cfg(target_os = "linux")]
-pub(crate) mod linux_prompts;
 #[cfg(not(target_os = "linux"))]
 pub(crate) mod only_instance;
-//mod open_listener;
 
 pub use app_menus::*;
 use breadcrumbs::Breadcrumbs;
