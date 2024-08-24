f63220a0dab89ab71ec19b06742c23c6a20aa2fc
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-08-23 19:17:42 -0700

    mw

--- crates/zed/build.rs
+++ crates/zed/build.rs
@@ -43,26 +43,4 @@ fn main() {
             }
         }
     }
-
-    #[cfg(target_os = "windows")]
-    {
-        #[cfg(target_env = "msvc")]
-        {
-            // todo(windows): This is to avoid stack overflow. Remove it when solved.
-            println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
-        }
-
-        let icon = std::path::Path::new("resources/windows/app-icon.ico");
-        println!("cargo:rerun-if-changed={}", icon.display());
-
-        let mut res = winresource::WindowsResource::new();
-        res.set_icon(icon.to_str().unwrap());
-        res.set("FileDescription", "Zed");
-        res.set("ProductName", "Zed");
-
-        if let Err(e) = res.compile() {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
-    }
 }
